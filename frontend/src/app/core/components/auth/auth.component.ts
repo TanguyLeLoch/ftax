dc09746f04f756a30c30b13ae8b188d6efbe1c81
Tanguy Le Loch <lelochtanguy@gmail.com>
2024-10-12 09:47:16 +0700

    delete logs

--- frontend/src/app/core/components/auth/auth.component.ts
+++ frontend/src/app/core/components/auth/auth.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from "@angular/router";
-import { AuthControllerService, AuthResponse } from "../../model";
-import { catchError, of } from "rxjs";
-import { ToastService } from "../../services/toast.service";
-import { CookieService } from "ngx-cookie-service";
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {AuthControllerService, AuthResponse} from "../../model";
+import {catchError, of} from "rxjs";
+import {ToastService} from "../../services/toast.service";
+import {CookieService} from "ngx-cookie-service";
 
 @Component({
   selector: 'app-auth',
@@ -33,24 +33,18 @@ export class AuthComponent implements OnInit {
 
       this.authController.verifyHash(email, hash).pipe(
         catchError((error) => {
-          console.error('Error occurred during hash verification', error);
           this.toastService.showToast('error', 'Error occurred during hash verification');
           this.router.navigate(['/login']);
           // Redirect to login on error
           return of("error"); // Return a safe value or an empty observable to prevent further actions
         })
       ).subscribe(result => {
-        console.log('Result', result);
         if (result === 'error') {
           return;
         }
         const response = result as AuthResponse
         this.cookieService.set('hash', response.hash);
         this.cookieService.set('email', email);
-        console.log('set cookie' + email);
-        console.log('Email', email);
-        console.log('Hash', response.hash);
-        console.log(response);
         this.router.navigate(['/']);
       });
     });
