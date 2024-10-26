ea445b831d9a798173026a830afef869e9a93791
Tanguy Le Loch <lelochtanguy@gmail.com>
2024-10-26 11:11:26 +0700

    init filter

---
+++ frontend/src/app/transaction/components/tx-filter/tx-filter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tx-filter',
+  templateUrl: './tx-filter.component.html',
+  styleUrl: './tx-filter.component.scss'
+})
+export class TxFilterComponent {
+
+}
